5feb29d38d09534e356740633a41f7f32e25b593
Sigal Raab <44254115+sigal-raab@users.noreply.github.com>
2025-03-26 18:18:07 +0200

    fix download link to t2m evaluators

--- prepare/download_t2m_evaluators.sh
+++ prepare/download_t2m_evaluators.sh
@@ -1,5 +1,6 @@
 echo -e "Downloading T2M evaluators"
-gdown --fuzzy https://drive.google.com/file/d/1DSaKqWX2HlwBtVH5l7DdW96jeYUIXsOP/view
+# gdown --fuzzy https://drive.google.com/file/d/1DSaKqWX2HlwBtVH5l7DdW96jeYUIXsOP/view
+gdown --fuzzy https://drive.google.com/file/d/1O_GUHgjDbl2tgbyfSwZOUYXDACnk25Kb/view
 rm -rf t2m
 
 unzip t2m.zip
